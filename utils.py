5c287c54d4d7b23e27042c405ba2af28eff57502
Tahira Kazimi <e2415354@ceng.metu.edu.tr>
2024-05-11 09:01:11 +0300

    write enabled

--- utils.py
+++ utils.py
@@ -56,7 +56,8 @@ def parse_data(outputs):
   return batched_outputs, pred_class_list, pred_scores
 
 
-def pointrend_head(model, batched_outputs, batched_images, gt_labels, device, threshold=0.5):
+def pointrend_head(model, batched_outputs, batched_images, gt_labels, device, threshold=0.5, write_enabled=False):
+  os.makedirs('/content/generated_masks', exist_ok=True)
   batched_final_masks = []
   ind = 0
   undetected_inds = []
@@ -140,9 +141,9 @@ def pointrend_head(model, batched_outputs, batched_images, gt_labels, device, th
         masked_image = (masked_array * 255).astype(np.uint8)
         masked_image_bgr = cv2.cvtColor(masked_image, cv2.COLOR_GRAY2BGR)
         l = str(int(gt_labels[i]))
-        cv2.imwrite(os.path.join('/content/', f'mask_{labels_names[l]}_{ind + 1}_mask_binary.png'), masked_image_bgr)
-        print(f"foreground mask writte to {os.path.join('/content/', f'mask_{labels_names[l]}_{ind + 1}_mask_binary.png')}")
-        ind += 1
+        if write_enabled:
+          cv2.imwrite(os.path.join('/content/generated_masks/', f'mask_{labels_names[l]}_{i + 1}_mask_binary.png'), masked_image_bgr)
+          print(f"foreground mask writte to {os.path.join('/content/', f'mask_{labels_names[l]}_{i + 1}_mask_binary.png')}")
         if device == 'cuda':
            masked_image = torch.tensor(masked_image_bgr).to('cuda')
         else:
